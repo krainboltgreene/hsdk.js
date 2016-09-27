f484e1e0470454c07537b47e3dd4cbc61d3fdc64
Kurtis Rainbolt-Greene <me@kurtisrainboltgreene.name>
2016-09-27 10:00:55 -0700

    Showing off more of the functionality

--- example/client.js
+++ example/client.js
@@ -1,14 +1,39 @@
 import hsdk from "../transpiled/source"
 
-hsdk({
+const SDK = hsdk({
   protocol: "http",
   host: "hsdkjs.getsandbox.com",
   root: "v1/resources"
 })
-  .then((client) => {
-    return client
-      .accounts
-      .list()
-  })
-  .then((response) => response.json())
-  .then((value) => console.log(value))
+
+Promise.all([
+  SDK
+    .then((client) => client().accounts().list())
+    .then((response) => response.json())
+    .then((value) => console.log({message: "List", value})),
+
+  SDK
+    .then((client) => client().accounts().show({id: "1"}))
+    .then((response) => response.json())
+    .then((value) => console.log({message: "Show", value})),
+
+  SDK
+    .then((client) => {
+      return client
+        .accounts
+        .update({
+          id: "1",
+          body: JSON.stringify({
+            data: {
+              id: "1",
+              type: "accounts",
+              attributes: {
+                age: 29
+              }
+            }
+          })
+        })
+    })
+    .then((response) => response.json())
+    .then((value) => console.log({message: "Update", value}))
+])
