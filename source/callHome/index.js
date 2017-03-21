aab09156e0a75b610d6cb376d7ab0dc31dd6e88c
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2017-03-20 23:06:07 -0700

    Oh hell yes, no longer maintaining this mess

--- source/callHome/index.js
+++ source/callHome/index.js
@@ -1,15 +1,22 @@
 /* eslint-disable no-console */
 import {path} from "ramda"
 import {merge} from "ramda"
+import {prop} from "ramda"
+import {groupBy} from "ramda"
+import {indexBy} from "ramda"
+import {treeify} from "ramda-extra"
 
 import wrapResources from "../wrapResources"
-import treeify from "../treeify"
 import request from "../request"
 
 export default function callHome (client: Promise<AxoisResponseType>): Promise<Object> {
   return client
     .then(path(["data", "data"]))
     .then(wrapResources)
-    .then(treeify)
+    .then(treeify([
+      groupBy(prop("namespace")),
+      groupBy(prop("version")),
+      indexBy(prop("intent")),
+    ]))
     .then(merge({request}))
 }
